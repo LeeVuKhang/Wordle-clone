ce6f389097b9b2c4962e360fc4da7dacc7382860
LeeVuKhang <lvukhangpq@gmail.com>
2026-01-15 15:00:10 +0700

    first commit & init front-end

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Wordle Clone</h1>
+            <p>Frontend setup complete!</p>
+        </div>
+    )
+}
+
+export default App
